215c074f619d298689a10a933936c146e94b41d4
rikokko <noo_kum@yahoo.co.jp>
2021-08-01 09:18:22 +0900

    Fixes：Changed the code to be simpler and more readable than before.

--- lesson3.js
+++ lesson3.js
@@ -1,15 +1,13 @@
 const ul = document.getElementById('js-list');
-for(let i=1; i<=2; i++) {
+const imgbox = ['bookmark.png','message.png'];
+const fragment = document.createDocumentFragment();
+for(let i=1; i<=imgbox.length; i++) {
     const li = document.createElement('li');
     const a = document.createElement('a');
     a.textContent = `a${i}`;
     a.href= `a${i}.html`;
     const img = document.createElement('img');
-    const imgbox = ['/img/bookmark.png','/img/message.png'];
-    img.src = imgbox[i-1];
-    const fragment = document.createDocumentFragment();
-    {
-      fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);
-    }
-    ul.appendChild(fragment)
+    img.src = `/img/${imgbox[i-1]}`;
+    {fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);}
 }
+ul.appendChild(fragment)
