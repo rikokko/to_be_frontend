e0528850e571891f5ac4521f54bc47802312cf03
rikokko <noo_kum@yahoo.co.jp>
2021-09-18 07:24:28 +0900

    then fix

--- lesson5.js
+++ lesson5.js
@@ -5,25 +5,28 @@
     ];
   const fragment = document.createDocumentFragment();
 
-  function list() {
+  function makeLinkList() {
     return new Promise((resolve) => {
-      resolve(info);
+      resolve();
     });
   }
-  list().then(() => { 
 
-      for(let i=0; i<=info.length-1; i++) {
-          const li = document.createElement('li');
-          const a = document.createElement('a');
-          const img = document.createElement('img');
-          a.textContent = info[i].text;
-          a.href = info[i].to;
-          img.src = info[i].img;
-          img.alt = info[i].alt;
-          fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);
-      }
-          ul.appendChild(fragment)
+  function ListContents(){
+    for(let i=0; i<=info.length-1; i++) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      const img = document.createElement('img');
+      a.textContent = info[i].text;
+      a.href = info[i].to;
+      img.src = info[i].img;
+      img.alt = info[i].alt;
+      fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);
+  }
+      ul.appendChild(fragment)
+  };
 
+  makeLinkList().then(() => { 
+    ListContents();
   });
 
 
