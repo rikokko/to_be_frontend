fbd7890f83c0f837c02d41c41ecf645e75f653a1
rikokko <noo_kum@yahoo.co.jp>
2021-09-20 09:31:46 +0900

    comment fix try

--- lesson5.js
+++ lesson5.js
@@ -1,33 +1,30 @@
-  const ul = document.getElementById('js-list');
-  const info = [
-    {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
-    {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
-    ];
-  const fragment = document.createDocumentFragment();
+const ul = document.getElementById('js-list');
+const info = [
+  {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+  {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+  ];
+const fragment = document.createDocumentFragment();
 
-  function ListContents(){
-    for(let i=0; i<=info.length-1; i++) {
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-      const img = document.createElement('img');
-      a.textContent = info[i].text;
-      a.href = info[i].to;
-      img.src = info[i].img;
-      img.alt = info[i].alt;
-      fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);
-  }
-      ul.appendChild(fragment)
-  };
+function ListContents(hoge){
+  for(let i=0; i<=hoge.length-1; i++) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+    a.textContent = hoge[i].text;
+    a.href = hoge[i].to;
+    img.src = hoge[i].img;
+    img.alt = hoge[i].alt;
+    fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);
+}
+    ul.appendChild(fragment)
+};
 
-  function makeLinkList() {
-    return new Promise((resolve,reject) => {
-      resolve(ListContents());
-    });
-  }
-
-  makeLinkList().then(() => { 
-     console.log('成功！');
+function makeLinkList() {
+  return new Promise((resolve) => {
+    resolve(info);
   });
+}
 
-
-
+makeLinkList().then((info) => { 
+  ListContents(info)
+});
