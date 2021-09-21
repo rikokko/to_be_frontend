8337d571f7b703f47b4269a9b005abb6c2bb36a9
rikokko <noo_kum@yahoo.co.jp>
2021-09-22 05:56:01 +0900

    Change argument name

--- lesson5.js
+++ lesson5.js
@@ -3,17 +3,18 @@ const info = [
   {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
   {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
   ];
+
 const fragment = document.createDocumentFragment();
 
-function ListContents(hoge){
-  for(let i=0; i<=hoge.length-1; i++) {
+function ListContents(add){
+  for(let i=0; i<=add.length-1; i++) {
     const li = document.createElement('li');
     const a = document.createElement('a');
     const img = document.createElement('img');
-    a.textContent = hoge[i].text;
-    a.href = hoge[i].to;
-    img.src = hoge[i].img;
-    img.alt = hoge[i].alt;
+    a.textContent = add[i].text;
+    a.href = add[i].to;
+    img.src = add[i].img;
+    img.alt = add[i].alt;
     fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);
 }
     ul.appendChild(fragment)
@@ -25,6 +26,6 @@ function makeLinkList() {
   });
 }
 
-makeLinkList().then((info) => { 
+makeLinkList().then(() => { 
   ListContents(info)
 });
