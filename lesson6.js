79c261809c787c74eaad4e9aedac473d580a1c89
rikokko <noo_kum@yahoo.co.jp>
2021-10-06 01:39:49 +0900

    Created and functionalized the makeLoadingImage function

--- lesson6.js
+++ lesson6.js
@@ -1,11 +1,12 @@
 const ul = document.getElementById('js-list');
 const fragment = document.createDocumentFragment();
 
-const loadingImg = document.createElement('img');
-loadingImg.src = "loading-circle.gif";
-loadingImg.id = 'loading-img'
-ul.appendChild(loadingImg);
-const loading = document.getElementById('loading-img');
+function makeLoadingImage(){
+  const loadingImg = document.createElement('img');
+  loadingImg.src = "loading-circle.gif";
+  loadingImg.id = 'loading-img'
+  ul.appendChild(loadingImg);
+}
 
 const info = [
   {to: "bookmark.html", img: "bookmark-icon.png", alt:"画像1", text: "ブックマーク"},
@@ -13,12 +14,14 @@ const info = [
   ];
 
 function makeLinkList() {
+    makeLoadingImage();
   return new Promise((resolve) => {
     setTimeout(() => resolve(info), 3000)
   });
 }
 
 makeLinkList().then((item) => {
+      const loading = document.getElementById('loading-img');
       loading.remove();
     for (let i = 0; i <= item.length - 1; i++) {
       const li = document.createElement('li');
