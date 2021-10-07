cbd9750d8dda96f2c9a5af0902bf5ecfa4b5e76f
rikokko <noo_kum@yahoo.co.jp>
2021-10-07 23:07:52 +0900

    I made a function to delete the loading image

--- lesson6.js
+++ lesson6.js
@@ -1,28 +1,31 @@
 const ul = document.getElementById('js-list');
 const fragment = document.createDocumentFragment();
-
-function makeLoadingImage(){
-  const loadingImg = document.createElement('img');
-  loadingImg.src = "loading-circle.gif";
-  loadingImg.id = 'loading-img'
-  ul.appendChild(loadingImg);
-}
-
 const info = [
   {to: "bookmark.html", img: "bookmark-icon.png", alt:"画像1", text: "ブックマーク"},
   {to: "message.html", img: "mail-icon.png", alt:"画像2", text: "メッセージ"}
   ];
 
+function makeLoadingImage(){
+    const loadingImg = document.createElement('img');
+    loadingImg.src = "loading-circle.gif";
+    loadingImg.id = 'loading-img'
+    ul.appendChild(loadingImg);
+  }
+
+function deleteLoadingImg() {
+   const loading = document.getElementById('loading-img');
+   loading.remove();
+}
+
 function makeLinkList() {
-    makeLoadingImage();
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(info), 3000)
-  });
+  makeLoadingImage();
+return new Promise((resolve) => {
+  setTimeout(() => resolve(info), 3000)
+});
 }
 
 makeLinkList().then((item) => {
-      const loading = document.getElementById('loading-img');
-      loading.remove();
+      deleteLoadingImg();
     for (let i = 0; i <= item.length - 1; i++) {
       const li = document.createElement('li');
       const a = document.createElement('a');
