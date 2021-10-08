65bfae0540ea7802dec50f2e6fb14f07e33df5f4
rikokko <noo_kum@yahoo.co.jp>
2021-10-08 15:43:12 +0900

    The code was automatically formatted

--- lesson6.js
+++ lesson6.js
@@ -1,41 +1,41 @@
 const ul = document.getElementById('js-list');
 const fragment = document.createDocumentFragment();
 const info = [
-  {to: "bookmark.html", img: "bookmark-icon.png", alt:"画像1", text: "ブックマーク"},
-  {to: "message.html", img: "mail-icon.png", alt:"画像2", text: "メッセージ"}
-  ];
+  { to: "bookmark.html", img: "bookmark-icon.png", alt: "画像1", text: "ブックマーク" },
+  { to: "message.html", img: "mail-icon.png", alt: "画像2", text: "メッセージ" }
+];
 
-function makeLoadingImg(){
-    const loadingImg = document.createElement('img');
-    loadingImg.src = "loading-circle.gif";
-    loadingImg.id = 'loading-img'
-    ul.appendChild(loadingImg);
-  }
+function makeLoadingImg() {
+  const loadingImg = document.createElement('img');
+  loadingImg.src = "loading-circle.gif";
+  loadingImg.id = 'loading-img'
+  ul.appendChild(loadingImg);
+}
 
 function deleteLoadingImg() {
-   const loading = document.getElementById('loading-img');
-   loading.remove();
+  const loading = document.getElementById('loading-img');
+  loading.remove();
 }
 
 function LoadingForMakeList() {
   makeLoadingImg();
-return new Promise((resolve) => {
-  setTimeout(() => resolve(info), 3000)
-});
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(info), 3000)
+  });
 }
 
 LoadingForMakeList().then((item) => {
-      deleteLoadingImg();
-      const fragment = document.createDocumentFragment();
-    for (let i = 0; i <= item.length - 1; i++) {
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-      const img = document.createElement('img');
-      a.textContent = item[i].text;
-      a.href = item[i].to;
-      img.src = item[i].img;
-      img.alt = item[i].alt;
-      fragment.appendChild(li).appendChild(a).appendChild(img);
-    }
-    ul.appendChild(fragment)
+  deleteLoadingImg();
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i <= item.length - 1; i++) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+    a.textContent = item[i].text;
+    a.href = item[i].to;
+    img.src = item[i].img;
+    img.alt = item[i].alt;
+    fragment.appendChild(li).appendChild(a).appendChild(img);
+  }
+  ul.appendChild(fragment)
 });
