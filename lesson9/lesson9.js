89d057a129ace8e76ff92fe4f9f992fd68f8bb89
rikokko <noo_kum@yahoo.co.jp>
2021-11-27 16:09:27 +0900

    refactor:Change the location where images are displayed and deleted

--- lesson9/lesson9.js
+++ lesson9/lesson9.js
@@ -3,20 +3,13 @@ const listItems = [
   { to: "bookmark.html", img: "bookmark-icon.png", alt: "画像1", text: "ブックマーク" },
   { to: "message.html", img: "mail-icon.png", alt: "画像2", text: "メッセージ" }
 ];
-
-function showLoadingImg() {
-  const loadingImg = document.createElement('img');
-  loadingImg.src = "loading-circle.gif";
-  loadingImg.id = 'loading-img'
-  ul.appendChild(loadingImg);
-}
-
-function deleteLoadingImg() {
-  const loading = document.getElementById('loading-img');
-  loading.remove();
-}
+const loadingImg = document.createElement('img');
+loadingImg.src = "loading-circle.gif";
+loadingImg.id = "loading-img";
+ul.appendChild(loadingImg);
 
 function createList(item) {
+  //リストを表示する
   const fragment = document.createDocumentFragment();
   for (let i = 0; i <= item.length - 1; i++) {
     const li = document.createElement('li');
@@ -29,16 +22,17 @@ function createList(item) {
     fragment.appendChild(li).appendChild(a).appendChild(img);
   }
   ul.appendChild(fragment)
+  //ローディング画像を削除する
+  const loadingID = document.getElementById("loading-img");
+  loadingID.remove();
 }
 
 async function fetchListData() {
-  showLoadingImg()
   const promise = new Promise((resolve) => {
     setTimeout(() => resolve(listItems), 3000)
   })
   const item = await promise;
   createList(item)
-  deleteLoadingImg()
 }
 
 fetchListData()
